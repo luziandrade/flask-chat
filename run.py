f1a28f83a304016c3b5e017ab6bcbf5287dd47aa
Luana Andrade <luzi015@hotmail.com>
2019-09-08 09:25:27 +0100

    creating users

--- run.py
+++ run.py
@@ -1,30 +1,41 @@
 import os
 from datetime import datetime
-from flask import Flask, redirect
+from flask import Flask, redirect, render_template, request, session
 
 app = Flask(__name__)
+app.secret_key = "randomstring123"
 messages = []
+
 
 def add_messages(username, message):
     """Add messages to the messages list"""
     now = datetime.now().strftime("%H:%M:%S")
     messages.append("({}) {} : {}".format(now, username, message))
 
+
 def get_all_messages():
     """Get all messages and separate using with br"""
     return "<br>".join(messages)
 
 
-
-@app.route('/')
+@app.route("/", methods=["GET", "POST"])
 def index():
     """main page with instructions"""
-    return 'To send a message use /USERNAME/MESSAGE'
+
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+
+    if "username" in session:
+        return redirect(session["username"])
+
+    return render_template("index.html")
+
 
 @app.route('/<username>')
 def user(username):
     """Display chat messages, its taking messages from list"""
     return '<h1>Welcome, {0}</h1> {1}'.format(username, get_all_messages())
+
 
 @app.route('/<username>/<message>')
 def send_message(username, message):
@@ -33,7 +44,5 @@ def send_message(username, message):
     return redirect("/" + username)
 
 
-
 if __name__ == '__main__':
     app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
-
